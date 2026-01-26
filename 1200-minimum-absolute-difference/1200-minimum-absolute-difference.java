e70a387f8624572c076495aa613aabd83a507cbb
Mayank Singh Rajpoot <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-01-26 22:54:59 +0530

    Time: 21 ms (72.27%), Space: 64 MB (46.18%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,42 +1,8 @@
 class Solution {
-    static void mergeSort(int[] a, int left, int right) {
-        if (left >= right) return;
-
-        int mid = (left + right) / 2;
-
-        // recursive calls (still same function)
-        mergeSort(a, left, mid);
-        mergeSort(a, mid + 1, right);
-
-        // merge step (written directly here)
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-
-        while (i <= mid && j <= right) {
-            if (a[i] <= a[j]) {
-                temp[k++] = a[i++];
-            } else {
-                temp[k++] = a[j++];
-            }
-        }
-
-        while (i <= mid) {
-            temp[k++] = a[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = a[j++];
-        }
-
-        // copy back
-        for (i = 0; i < temp.length; i++) {
-            a[left + i] = temp[i];
-        }
-    }
+    
     public List<List<Integer>> minimumAbsDifference(int[] a) {
         List<List<Integer>> nl = new ArrayList<>();
-        mergeSort(a, 0, a.length - 1);
-        
+        Arrays.sort(a);
         int min=a[1]-a[0];
         for(int i=0;i<a.length-1;i++){
             if(a[i+1]-a[i]<min){
