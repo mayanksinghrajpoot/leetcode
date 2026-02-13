93dd9dbc1d217174cf5aa0e0dac7cb073c5c74a7
Mayank Singh Rajpoot <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-02-14 01:04:08 +0530

    Time: 2 ms (71.14%), Space: 55.9 MB (16.51%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int c=1;
+        int val=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(c==0){
+                val=nums[i];
+                c++;
+                }
+            else if(nums[i]==val){
+                c++;
+            }else if (nums[i]!=val){
+                c--;
+            }
+        }
+        return val;
+    }
+}
