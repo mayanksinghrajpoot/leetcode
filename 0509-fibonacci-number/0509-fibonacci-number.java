01aafdbcab90954027eb4d50c2936f86895d4a3c
Mayank Singh Rajpoot <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-02-13 16:49:29 +0530

    Time: 0 ms (100%), Space: 42.3 MB (13.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,11 @@
 class Solution {
-    int solve(int n,ArrayList<Integer>dp){
-        if(n<=1)return n;
-        if(!dp.get(n).equals(-1))return dp.get(n);
-        int result = solve(n-1,dp)+solve(n-2,dp);
-        dp.set(n,result);
-        return result;
-
-    }
     public int fib(int n) {
-        if(n==1 || n==0){
-            return n;
+        if(n<=1)return n;
+        int arr[]= new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
         }
-        ArrayList<Integer> dp =new ArrayList<>(Collections.nCopies(n+1,-1));
-        return solve(n,dp);
-
-    }
-}
+        return arr[n];
+}}
