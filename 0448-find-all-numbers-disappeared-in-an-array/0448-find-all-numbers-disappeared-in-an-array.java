268a41f9211a0e62b58d7d333c91f6964f078330
Mayank Singh Rajpoot <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-02-17 15:20:15 +0530

    Time: 6 ms (79.48%), Space: 67.2 MB (63.18%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        // Step 1: Mark visited indices by negating values
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        
+        // Step 2: Collect indices that were never negated
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result;
+    }
+}
