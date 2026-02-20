c843bbc53349022c07b9ef2c695da1357a726bac
Mayank Singh Rajpoot <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-02-21 00:14:34 +0530

    Time: 0 ms (100%), Space: 42.9 MB (71.71%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,7 +1,12 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        // Since the string starts with '1', any second segment 
-        // must be preceded by at least one '0'.
-        return !s.contains("01");
+        for (int i = 1; i < s.length(); i++) {
+            // If we find a '1' that follows a '0', 
+            // a new segment of ones has started.
+            if (s.charAt(i - 1) == '0' && s.charAt(i) == '1') {
+                return false;
+            }
+        }
+        return true;
     }
 }
