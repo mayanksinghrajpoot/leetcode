ff5dbcaf7f372138486c215e0949468173580f6a
Kumar Mayank <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-04-06 16:03:23 +0530

    Time: 0 ms (100%), Space: 48.2 MB (81.27%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,27 @@
 class Solution {
+    public void recur(char[] s,int i,int j){
+        while(i<j){
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;}
+        return;
+    }
     public void reverseString(char[] s) {
-        int l=s.length -1;
-        int f=0;
-        for(int i=0;i<(s.length/2);i++){
-            char c=s[l];
-            s[l]=s[i];
-            s[i]=c;
-            l--;
-            f++;
-        }
+        recur(s,0,s.length-1);
     }
 }
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int l=s.length -1;
+//         int f=0;
+//         for(int i=0;i<(s.length/2);i++){
+//             char c=s[l];
+//             s[l]=s[i];
+//             s[i]=c;
+//             l--;
+//             f++;
+//         }
+//     }
+// }
