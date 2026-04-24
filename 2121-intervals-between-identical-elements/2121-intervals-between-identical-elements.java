102d26cf2f087db4c422e7f0c4ecada3100e421e
Kumar Mayank <152100239+mayanksinghrajpoot@users.noreply.github.com>
2026-04-25 04:22:23 +0530

    Time: 45 ms (62.26%), Space: 172.4 MB (74.29%) - LeetHub

---
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long[] getDistances(int[] nums) {
+        long[] arr=new long[nums.length];
+        HashMap<Integer,ArrayList<Integer>> list=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            list.put(nums[i],new ArrayList<>());
+        }
+        for(int i=0;i<nums.length;i++){
+            list.get(nums[i]).add(i);
+        }
+        for(ArrayList<Integer> indices:list.values()){
+            int groupSize = indices.size();
+            long totalGroupSum = 0;
+        for (int idx : indices) totalGroupSum += idx;
+
+        long runningLeftSum = 0;
+
+        for (int i = 0; i < groupSize; i++) {
+            long currIdx = indices.get(i);
+            long rightSum = totalGroupSum - runningLeftSum - currIdx;
+            long leftTotalDist = (i * currIdx) - runningLeftSum;
+            long rightTotalDist = rightSum - (long)(groupSize - i - 1) * currIdx;
+            arr[(int)currIdx] = leftTotalDist + rightTotalDist;
+            runningLeftSum += currIdx;
+        }}
+        
+        return arr;
+    }
+}
+        
